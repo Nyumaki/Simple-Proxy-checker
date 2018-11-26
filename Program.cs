8d00a7b95d20b445ccb89af8e93c0b3bc5b32926
Ryuuu <ryuking79@gmail.com>
2018-11-26 10:47:19 +0000

    lots of stuff

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Security;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SimpleProxyChecker
+{
+    class Program
+    {
+        public static IList<Proxy> proxyList = new List<Proxy>();
+        static void Main()
+        {
+            Console.Title = "Proxy Checker Made By Ryuuu";
+            ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback( delegate { return true; });
+            string proxys = new WebClient().DownloadString("http://proxy.l337.tech/txt");
+            using (StringReader reader = new StringReader(proxys))
+                AddProxy(reader);
+            Console.WriteLine("Made by Ryuuu for vrchat use");
+            Console.ReadLine();
+            TestProxyList();
+            Console.ReadLine();
+        }
+
+        private static void AddProxy(StringReader reader)
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                Proxy p = Proxy.Parse(line);
+                if (p != null)
+                {
+                    proxyList.Add(p);
+                }
+            }
+        }
+
+        public static int working = 0;
+        public static int bad = 0;
+        public static void TestProxyList()
+        {
+            int proxyNum = proxyList.Count;
+            int proxyTested = 0;
+            
+            Task.Factory.StartNew(() =>
+            {
+                Parallel.ForEach(proxyList, new ParallelOptions() { MaxDegreeOfParallelism = 32 }, proxy =>
+                {
+                    proxy.PerformTest();
+                    ++proxyTested;
+                    Console.Title = "Proxy Checker Made By Ryuuu - Num of proxy Tested: " + proxyTested + " Working Proxy: " + working + " Bad Proxy: " + bad;
+                });
+            });
+        }
+
+
+    }
+}
---
+++ Proxy.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleProxyChecker
+{
+    public class Proxy
+    {
+        public IPEndPoint IPEndPoint { get; set; }
+
+        public Proxy(IPEndPoint endPoint) => IPEndPoint = endPoint;
+
+        public static Proxy Parse(string str)
+        {
+            str = str.Replace(';', ':');
+            str = str.Replace(',', ':');
+            string[] parts = str.Split(':');
+
+            try
+            {
+                string ipStr = parts[0];
+                string portStr = parts[1];
+                IPAddress.TryParse(ipStr, out IPAddress ip);
+
+                return new Proxy(new IPEndPoint(ip, int.Parse(portStr)));
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+        public void PerformTest()
+        {
+            TestProxy(this);
+        }
+
+        public static void TestProxy(Proxy proxy)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://www.vrchat.net/");
+            request.Proxy = new WebProxy(proxy.IPEndPoint.Address.ToString(), proxy.IPEndPoint.Port);
+            request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36";
+            request.Timeout = 2000;
+
+            try
+            {
+                WebResponse response = request.GetResponse();
+                Console.WriteLine(proxy.IPEndPoint.Address.ToString() + ":" + proxy.IPEndPoint.Port);
+                Program.working++;
+            }
+            catch (Exception)
+            {
+                Program.bad++;
+            }
+        }
+    }
+}
